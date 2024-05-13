0ef23d97b5d57dd78ec948eba5dd423197b0bd3c
GUODnuli <624115642@qq.com>
2024-05-14 00:23:30 +0800

    Small ball Move Function.

---
+++ Assets/Scripts/4.UnityInput/小球场景/MoveManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal") * 2.5f, 0, 0);
+        transform.Translate(direction * Time.deltaTime);
+    }
+}
