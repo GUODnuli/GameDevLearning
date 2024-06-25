3bc52b0ef244e237eaa1161976b082ca54597909
zhuangxiaosen <624115642@qq.com>
2024-06-25 17:48:27 +0800

    方块保卫战,进行中.

---
+++ Assets/Scripts/6.Unity3D/Hold Cube/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void Move()
+    {
+        Vector3 offset = transform.forward * Time.fixedDeltaTime;
+    }
+}
